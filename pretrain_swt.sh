cf3b94736cddb760324b312f22dc5577e609d932
andylau <1173602856@qq.com>
2025-06-28 19:27:36 +0800

    nccl debug

--- pretrain_swt.sh
+++ pretrain_swt.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export NCCL_DEBUG=INFO
 python -u pretrain_swt.py --dataset_path datasets/ant \
  --encoder_path weights/pretrain_swt_cnn_r2_0409_large \
  --encoder_output_path weights/encoder_ant_0627 \
