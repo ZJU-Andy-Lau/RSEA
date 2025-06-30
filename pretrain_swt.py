04c38ffc6d0c5525dccd0768a742f546c8380068
andylau <1173602856@qq.com>
2025-06-30 15:12:04 +0800

    debug

--- pretrain_swt.py
+++ pretrain_swt.py
@@ -145,7 +145,8 @@ def pretrain(args):
     scaler = GradScaler()
     os.makedirs('./log',exist_ok=True)
     logger = TableLogger('./log',['epoch','loss','loss_obj','loss_height','loss_conf','loss_feat','loss_dis','k','lr_encoder','lr_decoder'],'finetune_log')
-        
+    
+    torch.autograd.set_detect_anomaly(True)
     for epoch in range(args.max_epoch):
         print(f'Epoch:{epoch}')
         sampler.set_epoch(epoch)
