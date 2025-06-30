7d8b82b3efc21fa6140cce7e42c32dac02938c6a
andylau <1173602856@qq.com>
2025-06-30 17:08:13 +0800

    debug

--- criterion.py
+++ criterion.py
@@ -161,10 +161,12 @@ class CriterionFinetuneNormal(nn.Module):
                 H,W
                 ):
         
+        print("2-1:",detect_nan([feat1_PD,feat2_PD,pred1_P3,pred2_P3,conf1_P,conf2_P,obj_P3,residual1_P,residual2_P]))
+
         feat1_PD,feat2_PD,pred1_P3,pred2_P3,conf1_P,conf2_P,obj_P3,residual1_P,residual2_P = \
             feat1_PD.to(torch.float32),feat2_PD.to(torch.float32),pred1_P3.to(torch.float32),pred2_P3.to(torch.float32),conf1_P.to(torch.float32),conf2_P.to(torch.float32),obj_P3.to(torch.float32),residual1_P.to(torch.float32),residual2_P.to(torch.float32)
 
-        # print("2:",detect_nan([feat1_PD,feat2_PD,pred1_P3,pred2_P3,conf1_P,conf2_P,obj_P3,residual1_P,residual2_P]))
+        print("2-2:",detect_nan([feat1_PD,feat2_PD,pred1_P3,pred2_P3,conf1_P,conf2_P,obj_P3,residual1_P,residual2_P]))
 
         P = H*W
         robust_mask = (residual1_P <= self.residual_thresholds) & (residual2_P <= self.residual_thresholds)
@@ -190,7 +192,7 @@ class CriterionFinetuneNormal(nn.Module):
         loss_conf = .5 * self.bce(conf1_P[conf_valid1],conf1_gt_P[conf_valid1]) + .5 * self.bce(conf2_P[conf_valid2],conf2_gt_P[conf_valid2])
         loss_conf = loss_conf * 1000 * min(1.,epoch / 3.)
 
-        # print("3:",detect_nan([loss_obj,loss_height,loss_conf]))
+        print("3:",detect_nan([loss_obj,loss_height,loss_conf]))
 
         loss_obj = loss_obj.mean()
         loss_height = loss_height.mean()
@@ -204,7 +206,7 @@ class CriterionFinetuneNormal(nn.Module):
         simi_negative = torch.concatenate([torch.sum(feat1_PD[robust_mask] * torch.roll(feat2_PD,shift_amount)[robust_mask],dim=1),
                                            torch.sum(feat2_PD[robust_mask] * torch.roll(feat1_PD,shift_amount)[robust_mask],dim=1)])
         
-        # print("4:",detect_nan([simi_positive,simi_negative]))
+        print("4:",detect_nan([simi_positive,simi_negative]))
         loss_feat = torch.clip(1. - simi_positive,min=0.).mean() * 10000. + torch.clip(simi_negative - 7.,min=0).mean() * 10000.
 
         loss = loss_obj + loss_height + loss_conf + loss_feat
@@ -220,6 +222,7 @@ class CriterionFinetuneDis(nn.Module):
                 residual1_P,residual2_P,
                 residual_thresholds,
                 ):
+        print("5-1:",detect_nan(pred1_P3,pred2_P3,residual1_P,residual2_P))
         pred1_P3,pred2_P3,residual1_P,residual2_P = \
            pred1_P3.to(torch.float32),pred2_P3.to(torch.float32),residual1_P.to(torch.float32),residual2_P.to(torch.float32)
         robust_mask = (residual1_P <= residual_thresholds) & (residual2_P <= residual_thresholds)
@@ -227,7 +230,7 @@ class CriterionFinetuneDis(nn.Module):
         dis_obj = torch.norm(pred1_P3[robust_mask,:2] - pred2_P3[robust_mask,:2],dim=-1)
         dis_height = torch.abs(pred1_P3[robust_mask,2] - pred2_P3[robust_mask,2]) * 100
 
-        # print("5:",detect_nan([dis_obj,dis_height]))
+        print("5-2:",detect_nan([dis_obj,dis_height]))
 
         dis_obj = dis_obj.mean()
         dis_height = dis_height.mean()
