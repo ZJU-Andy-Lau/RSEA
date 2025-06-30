d144ca255eb4fe47380f78122fa520873c17f66e
andylau <1173602856@qq.com>
2025-06-30 20:32:11 +0800

    debug

--- model_new.py
+++ model_new.py
@@ -195,13 +195,13 @@ class Decoder(nn.Module):
             nn.ReLU(),
             nn.Conv2d(in_channels // 16,1,1,1,0)
         )
-        self.bn = bnac(in_channels)
+        # self.bn = bnac(in_channels)
 
 
     def forward(self, res):
         # res = res / torch.norm(res,dim=1,keepdim=True)
-        if self.use_bn:
-            res = self.bn(res)
+        # if self.use_bn:
+        #     res = self.bn(res)
         for block in self.blocks:
             x = block(res)
             res = res + x
