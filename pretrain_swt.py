80ff4e89e4fbae8de3cea5447da04a072ba03fce
andylau <1173602856@qq.com>
2025-07-04 13:36:06 +0800

    debug

--- pretrain_swt.py
+++ pretrain_swt.py
@@ -338,7 +338,7 @@ def pretrain(args):
 
             loss,loss_obj,loss_height,loss_conf,loss_feat,loss_dis,k,conf_mean = compute_loss(args,epoch,compose_data,encoder,decoder,projector,criterion)
 
-            if rank == 1 and epoch == 1:
+            if rank == 1 and epoch == 1 and iter_idx == 1:
                 loss = torch.tensor(torch.nan,device=loss.device)
             
 
@@ -349,7 +349,7 @@ def pretrain(args):
             dist.all_reduce(loss_status_tensor, op=dist.ReduceOp.SUM)
 
             if loss_status_tensor.item() > 0:
-                print(f"--- [Rank {rank}] 检测到 NaN！Epoch {epoch}, iter {iter_idx}. 所有进程将一起跳过此次更新。---")
+                pprint(f"--- 检测到 NaN！Epoch {epoch}, iter {iter_idx}. 所有进程将一起跳过此次更新。---")
                 del loss,loss_obj,loss_height,loss_conf,loss_feat,loss_dis,conf_mean
                 encoder_scheduler.step()
                 continue 
