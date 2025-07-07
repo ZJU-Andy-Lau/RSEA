40328b276ccf40c76b59dc15528b1bb6fba7ce85
andylau <1173602856@qq.com>
2025-07-07 14:52:11 +0800

    debug

--- dataloader.py
+++ dataloader.py
@@ -199,13 +199,13 @@ def process_image(
         
         local_selected = local_total[select_idx[:10000]]
 
-        if dist.get_rank() == 0:
-            test_idx1 = ((local_selected - np.array([y1,x1])) * r1).astype(int)
-            test_idx2 = ((local_selected - np.array([y2,x2])) * r2).astype(int)
-            test_obj1 = obj1[k][test_idx1[:,0],test_idx1[:,1]]
-            test_obj2 = obj2[k][test_idx2[:,0],test_idx2[:,1]]
-            dis_obj = np.linalg.norm(test_obj1 - test_obj2,axis=-1)
-            print(f"dis_obj: \t {dis_obj.min()} \t {dis_obj.max()} \t {dis_obj.mean()} \t {np.median(dis_obj)}")
+        # if dist.get_rank() == 0:
+        #     test_idx1 = ((local_selected - np.array([y1,x1])) * r1).astype(int)
+        #     test_idx2 = ((local_selected - np.array([y2,x2])) * r2).astype(int)
+        #     test_obj1 = obj1[k][test_idx1[:,0],test_idx1[:,1]]
+        #     test_obj2 = obj2[k][test_idx2[:,0],test_idx2[:,1]]
+        #     dis_obj = np.linalg.norm(test_obj1 - test_obj2,axis=-1)
+        #     print(f"dis_obj: \t {dis_obj.min()} \t {dis_obj.max()} \t {dis_obj.mean()} \t {np.median(dis_obj)}")
 
         corr_idxs1_list.append(np.clip((local_selected - np.array([y1,x1])) * r1 / downsample_ratio - np.array([.5,.5]),a_min=0.,a_max=output_size / downsample_ratio - 1.))
         corr_idxs2_list.append(np.clip((local_selected - np.array([y2,x2])) * r2 / downsample_ratio - np.array([.5,.5]),a_min=0.,a_max=output_size / downsample_ratio - 1.))
@@ -304,10 +304,10 @@ class PretrainDataset(Dataset):
         obj1 = torch.from_numpy(np.stack([downsample(obj,self.DOWNSAMPLE) for obj in obj1],axis=0)).to(torch.float32)
         obj2 = torch.from_numpy(np.stack([downsample(obj,self.DOWNSAMPLE) for obj in obj2],axis=0)).to(torch.float32)
 
-        # test_obj1 = sample_bilinear(obj1,overlaps_1)
-        # test_obj2 = sample_bilinear(obj2,overlaps_2)
-        # obj_dis = torch.norm(test_obj1 - test_obj2,dim=-1).reshape(-1)
-        # print(f"dis:{obj_dis.min().item()} \t {obj_dis.max().item()} \t {obj_dis.mean().item()} \t {obj_dis.median().item()}\n")
+        test_obj1 = sample_bilinear(obj1,overlaps_1)
+        test_obj2 = sample_bilinear(obj2,overlaps_2)
+        obj_dis = torch.norm(test_obj1 - test_obj2,dim=-1).reshape(-1)
+        print(f"dis:{obj_dis.min().item()} \t {obj_dis.max().item()} \t {obj_dis.mean().item()} \t {obj_dis.median().item()}\n")
 
         residual1 = np.stack([residual_average(residual,self.DOWNSAMPLE) for residual in residual1],axis=0)
         residual2 = np.stack([residual_average(residual,self.DOWNSAMPLE) for residual in residual2],axis=0)
