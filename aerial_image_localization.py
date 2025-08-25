aa240db317f93f5bc7c8e1dac3694dd6229cf7ae
andylau <1173602856@qq.com>
2025-08-25 16:09:43 +0800

    debug

--- aerial_image_localization.py
+++ aerial_image_localization.py
@@ -311,7 +311,7 @@ if __name__ == '__main__':
     print(f"avg sigma:{conf_score.mean()}")
 
     # _,mask = cv2.findHomography(local_linesamp,mu_linesamp,cv2.RANSAC,ransacReprojThreshold=2. * conf_score.mean())
-    _,mask = cv2.estimateAffine2D(local_linesamp,mu_linesamp,cv2.RANSAC,ransacReprojThreshold=5. * conf_score.mean())
+    _,mask = cv2.estimateAffine2D(local_linesamp,mu_linesamp,cv2.RANSAC,ransacReprojThreshold=1. * conf_score.mean())
     inliers = mask.ravel() == 1
     outliers = mask.ravel() == 0
     print(f"inlier: {inliers.sum()}/{len(inliers)}")
