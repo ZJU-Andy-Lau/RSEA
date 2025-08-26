e5a728ac27f0d022c1296b793a0385c91205153f
andylau <1173602856@qq.com>
2025-08-26 19:25:38 +0800

    debug

--- utils.py
+++ utils.py
@@ -753,4 +753,4 @@ def vis_conf(conf:np.ndarray,img:np.ndarray,ds,output_path):
 
         cv2.circle(canvas,(p[1],p[0]),radius=1,color=color,thickness=-1)
     
-    cv2.imwrite(canvas,output_path)
+    cv2.imwrite(output_path,canvas)
