acc205a181777f1e710c8adc69d337a1af46e587
andylau <1173602856@qq.com>
2025-09-02 11:14:49 +0800

    debug

--- grid.py
+++ grid.py
@@ -347,8 +347,8 @@ class Grid():
 
                     features_pD = torch.sum(features_p3D * reverse_dists_ratio.unsqueeze(-1),dim=1).to(torch.float32)
                     confs_p1 = torch.sum(confs_p3 * reverse_dists_ratio,dim=1).to(torch.float32)
-                    objs_p3 = torch.sum(objs_p33 * dists_ratio.unsqueeze(-1),dim=1).to(torch.float32)
-                    locals_p2 = torch.sum(locals_p32 * dists_ratio.unsqueeze(-1),dim=1).to(torch.float32)
+                    objs_p3 = torch.sum(objs_p33 * reverse_dists_ratio.unsqueeze(-1),dim=1).to(torch.float32)
+                    locals_p2 = torch.sum(locals_p32 * reverse_dists_ratio.unsqueeze(-1),dim=1).to(torch.float32)
 
 
 
