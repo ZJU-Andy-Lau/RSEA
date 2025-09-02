de7f0d2db47fef9e3b73a3005ffbab12bb2df78c
andylau <1173602856@qq.com>
2025-09-02 19:51:42 +0800

    debug

--- rs_image.py
+++ rs_image.py
@@ -25,9 +25,10 @@ class RSImage():
         self.root = root
         self.id = id
         # self.image = self.__load_image__(os.path.join(root,'image.tif'))
+        
+        self.image = cv2.imread(os.path.join(root,'image.png'),cv2.IMREAD_GRAYSCALE)
         if options.use_clahe:
-            self.image = cv2.imread(os.path.join(root,'image.png'),cv2.IMREAD_GRAYSCALE)
-        self.image = clahe.apply(self.image)
+            self.image = clahe.apply(self.image)
         self.image = np.stack([self.image] * 3,axis=-1)
         self.dem = np.load(os.path.join(root,'dem.npy'))
         if os.path.exists(os.path.join(root,'tie_points.txt')):
