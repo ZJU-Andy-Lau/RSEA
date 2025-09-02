e00f5cc57b281db62a664eeb0ed3214143ee8ed6
andylau <1173602856@qq.com>
2025-09-02 15:13:22 +0800

    debug

--- adjust_zero.py
+++ adjust_zero.py
@@ -118,7 +118,7 @@ if __name__ == '__main__':
     # parser.add_argument('--lr_decay_per_100_epochs', type=float, default=0.85,
     #                     help='factor of lr decay in every 10 epochs')
     
-    parser.add_argument('--conf_threshold', type=float, default=0.7,
+    parser.add_argument('--conf_threshold', type=float, default=0.5,
                         help='minimum confidence to filter reliable patches')
     
     parser.add_argument('--ransac_threshold', type=int, default=20,
