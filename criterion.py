d3c3b1b0ca3ce0fca38b5cc9b25923069bb8e801
andylau <1173602856@qq.com>
2025-09-12 21:10:11 +0800

    loss feat * 10000 -> 1000

--- criterion.py
+++ criterion.py
@@ -237,9 +237,9 @@ class CriterionFinetune(nn.Module):
                                            torch.norm(feat2_PD - feat2_negative,dim=1)])
         
         # loss_feat = torch.clip(1. - simi_positive,min=0.).mean() * 10000. + torch.clip(simi_negative - .7,min=0).mean() * 10000.
-        loss_feat = torch.clip(simi_positive - simi_negative + 5.,min=0.).mean() * 10000
+        loss_feat = torch.clip(simi_positive - simi_negative + 5.,min=0.).mean() * 1000
 
-        print(f"feat dis mean:{(simi_negative - simi_positive).mean().item()}")
+        # print(f"feat dis mean:{(simi_negative - simi_positive).mean().item()}")
 
 
         loss = loss_obj + loss_height + loss_conf + loss_feat #+ loss_dis * max(min(1.,epoch / 5. - 1.),0.)
