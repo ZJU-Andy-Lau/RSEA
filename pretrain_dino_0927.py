8c7a9f32f390ae76a8f1a37a5560bf515085843c
andylau <1173602856@qq.com>
2025-09-29 09:56:13 +0800

    debug

--- pretrain_dino_0927.py
+++ pretrain_dino_0927.py
@@ -612,6 +612,22 @@ def pretrain(args):
                 logger.add_image('vis/train_img_12',train_img_12.astype(np.uint8),epoch,dataformats='HWC')
                 logger.add_image('vis/train_img_22',train_img_22.astype(np.uint8),epoch,dataformats='HWC')
 
+                cv2.imwrite('./vis/rank_1_img_1.png',train_img_1.astype(np.uint8))
+                cv2.imwrite('./vis/rank_1_img_2.png',train_img_2.astype(np.uint8))
+            
+        if dist.get_rank() == 1:
+            train_img_1,train_img_2 = img1[0][0].permute(1,2,0).detach().cpu().numpy(),img2[0][0].permute(1,2,0).detach().cpu().numpy()
+            train_img_1 = 255. * (train_img_1 - train_img_1.min()) / (train_img_1.max() - train_img_1.min())
+            train_img_2 = 255. * (train_img_2 - train_img_2.min()) / (train_img_2.max() - train_img_2.min())
+            cv2.imwrite('./vis/rank_2_img_1.png',train_img_1.astype(np.uint8))
+            cv2.imwrite('./vis/rank_2_img_2.png',train_img_2.astype(np.uint8))
+        
+        if dist.get_rank() == 2:
+            train_img_1,train_img_2 = img1[0][0].permute(1,2,0).detach().cpu().numpy(),img2[0][0].permute(1,2,0).detach().cpu().numpy()
+            train_img_1 = 255. * (train_img_1 - train_img_1.min()) / (train_img_1.max() - train_img_1.min())
+            train_img_2 = 255. * (train_img_2 - train_img_2.min()) / (train_img_2.max() - train_img_2.min())
+            cv2.imwrite('./vis/rank_3_img_1.png',train_img_1.astype(np.uint8))
+            cv2.imwrite('./vis/rank_3_img_2.png',train_img_2.astype(np.uint8))
         
             # logger.update({
             #     'epoch':epoch,
