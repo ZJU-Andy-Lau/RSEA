04db7dd494de5729039b736052fffb5eea84155d
andylau <1173602856@qq.com>
2025-10-01 23:07:35 +0800

    降低grid训练时特征噪声

--- grid.py
+++ grid.py
@@ -371,7 +371,7 @@ class Grid():
         min_photo_loss = 1e8
 
         patch_noise_buffer = F.normalize(torch.normal(mean=0.,std=1.,size=(1,self.encoder.output_channels,max_patch_num * 5,1)),dim=1).to(self.elements[0].buffer['features'].device)
-        patch_noise_amp = torch.rand(1,1,max_patch_num * 5,1,device=patch_noise_buffer.device,dtype=patch_noise_buffer.dtype) * .1 + .05
+        patch_noise_amp = torch.rand(1,1,max_patch_num * 5,1,device=patch_noise_buffer.device,dtype=patch_noise_buffer.dtype) * 0.01
         patch_noise_buffer = patch_noise_buffer * patch_noise_amp
 
         vis_flag = 0
