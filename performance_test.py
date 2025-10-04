d942dc5501fb1d071320230add07b4f6bcf95067
andylau <1173602856@qq.com>
2025-10-04 14:34:15 +0800

    debug

--- performance_test.py
+++ performance_test.py
@@ -126,7 +126,7 @@ def evaluate(args,decoder:DecoderFinetune,features,gt_objs,map_coeffs):
     output = decoder(features)
     output = output.permute(0,2,3,1).flatten(0,2)
     pred_obj = warp_by_poly(output,map_coeffs)
-    gt_objs = gt_objs.flatten(0,1)
+    gt_objs = gt_objs.flatten(0,1).cuda()
     dis = torch.norm(pred_obj - gt_objs,dim=1)
     print(f"dis: mean:{dis.mean().item():.2f} \t median:{dis.median().item():.2f} \t min:{dis.min().item():.2f} \t max:{dis.max().item():.2f}")
     visualize_subset_points(pred_obj.cpu().numpy(),gt_objs.cpu().numpy(),os.path.join(args.output_path,f"{args.test_name}_res.png"))
