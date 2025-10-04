49dcfc902f636b3cd2dc1d7ba202c37cf859dac7
andylau <1173602856@qq.com>
2025-10-04 23:27:31 +0800

    给1001用回pos_embed

--- model/encoder_dino_0927.py
+++ model/encoder_dino_0927.py
@@ -137,7 +137,7 @@ class Adapter(nn.Module):
             nn.Conv2d(self.output_channels,self.output_channels,1,1,0),
         )
 
-        # self.pos_encoder = PositionalEncoding(dim=output_channels)
+        self.pos_encoder = PositionalEncoding(dim=output_channels)
         
         # 自注意力模块，借鉴CasP中的TransformerBlock设计
         self.self_attention_block = AttentionBlock(dim=output_channels, num_heads=8)
@@ -154,9 +154,9 @@ class Adapter(nn.Module):
     def forward(self,x):
         raw_feat = self.cnn(x)
         B,D,H,W = raw_feat.shape
-        # feat_with_pos = self.pos_encoder(raw_feat)
-        # feat_seq = feat_with_pos.flatten(2).transpose(1,2)
-        feat_seq = raw_feat.flatten(2).transpose(1,2)
+        feat_with_pos = self.pos_encoder(raw_feat)
+        feat_seq = feat_with_pos.flatten(2).transpose(1,2)
+        # feat_seq = raw_feat.flatten(2).transpose(1,2)
         attn_output = self.self_attention_block(feat_seq, feat_seq, feat_seq)
         attended_sequence = self.norm(feat_seq + attn_output)
         feat = F.normalize(attended_sequence.transpose(1, 2).view(B, D, H, W),dim=1)
