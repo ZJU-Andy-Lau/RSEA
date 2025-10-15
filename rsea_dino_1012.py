0fc7e89e4acb249e2dab2c9248bb215b7363f061
andylau <1173602856@qq.com>
2025-10-15 11:10:36 +0800

    debug

--- rsea_dino_1012.py
+++ rsea_dino_1012.py
@@ -268,7 +268,7 @@ class RSEA():
         fitter = AffineFitter()
 
         total_num = len(src)
-        afm,mask = cv2.estimateAffine2D(src.cpu().numpy(),tgt_mu.cpu().numpy(),method=cv2.RANSAC,ransacReprojThreshold = avg_sigma)
+        afm,mask = cv2.estimateAffine2D(src.cpu().numpy(),tgt_mu.cpu().numpy(),method=cv2.RANSAC,ransacReprojThreshold = avg_sigma.cpu().item())
         inliers = mask.ravel() == 1
 
         src = src[inliers]
