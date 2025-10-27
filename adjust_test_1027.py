5753e601a00a2e3d6b392b1c888f2ca2f562fa4e
andylau <1173602856@qq.com>
2025-10-27 10:53:35 +0800

    debug

--- adjust_test_1027.py
+++ adjust_test_1027.py
@@ -808,7 +808,7 @@ def haversine_distance(coords1: np.ndarray, coords2: np.ndarray) -> np.ndarray:
     lon2_rad = np.radians(lon2)
 
     dlat = lat2_rad - lat1_rad
-    dlon = lon2_rad - lonl_rad
+    dlon = lon2_rad - lon1_rad
 
     a = np.sin(dlat / 2)**2 + np.cos(lat1_rad) * np.cos(lat2_rad) * np.sin(dlon / 2)**2
     c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
