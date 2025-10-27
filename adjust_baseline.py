34f13f6ed5febe9f71adc8362f7da02e48480268
andylau <1173602856@qq.com>
2025-10-27 16:57:18 +0800

    debug

--- adjust_baseline.py
+++ adjust_baseline.py
@@ -111,10 +111,11 @@ class TraditionalBundleAdjuster:
                 mkpts_j = results['keypoints1'].cpu().numpy()
                 conf = results['confidence'].cpu().numpy()
 
-                print(len(mkpts_i),img_i_data['patch_gray'].squeeze().cpu().numpy().shape)
-                cv2.imwrite('./vis/loftr_0.png',(255. * img_i_data['patch_gray'].squeeze().cpu().numpy()).astype(int))
-                cv2.imwrite('./vis/loftr_1.png',(255. * img_j_data['patch_gray'].squeeze().cpu().numpy()).astype(int))
-                exit()
+                if len(mkpts_i) > 100:
+                    print(len(mkpts_i),img_i_data['patch_gray'].squeeze().cpu().numpy().shape)
+                    cv2.imwrite('./vis/loftr_0.png',(255. * img_i_data['patch_gray'].squeeze().cpu().numpy()).astype(int))
+                    cv2.imwrite('./vis/loftr_1.png',(255. * img_j_data['patch_gray'].squeeze().cpu().numpy()).astype(int))
+                    exit()
                 
                 if len(mkpts_i) == 0:
                     continue
