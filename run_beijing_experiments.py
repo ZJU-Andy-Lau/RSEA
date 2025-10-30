26a15bad9a818d3a0da8d72e4a86ec320ba671ee
andylau <1173602856@qq.com>
2025-10-30 20:46:46 +0800

    debug

--- run_beijing_experiments.py
+++ run_beijing_experiments.py
@@ -71,7 +71,7 @@ def get_param_grid():
     
     # 1. 定义搜索空间
     param_space = {
-        'root': ['./datasets/beijing_test_error_5','./datasets/wv_test_error_10'], # <--- !! [修改] 新增, 请填入您的路径
+        'root': ['./datasets/beijing_test_error_5','./datasets/beijing_test_error_10'], # <--- !! [修改] 新增, 请填入您的路径
         'max_lr': [0.1, 0.05, 0.01],
         'window_size': [1000, 2000],
         'grid_num': [8, 16, 24],
