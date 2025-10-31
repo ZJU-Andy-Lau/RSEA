a7d499aa8ae2745ed5420792238ed0b388560467
andylau <1173602856@qq.com>
2025-10-31 20:01:11 +0800

    debug

--- criterion_0927.py
+++ criterion_0927.py
@@ -258,7 +258,7 @@ class CriterionFinetune(nn.Module):
 
 
         # loss_feat = torch.clip(1. - simi_positive,min=0.).mean() * 10000. + torch.clip(simi_negative - .7,min=0).mean() * 10000.
-        loss_feat = torch.clip(simi_negative - simi_positive + .5,min=0.).mean() * 10000 + (1. - simi_positive.mean()) * 20000
+        loss_feat = torch.clip(simi_negative - simi_positive + .5,min=0.).mean() * 10000 + (1. - simi_positive.mean()) * 5000
         # loss_feat = torch.clip(simi_positive - simi_negative + 2.,min=0.).mean() * 100 + simi_positive.mean() * 200 + feat_length * 100 \
         #             + torch.clip(simi_it_positive - simi_it_negative + 2.,min=0.).mean() * 100 + simi_it_positive.mean() * 200
         loss_feat_weight = 1.
