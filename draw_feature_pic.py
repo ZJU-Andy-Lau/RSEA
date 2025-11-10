55cb3e5b993c6d41de2e5070a905259d33a84c32
andylau <1173602856@qq.com>
2025-11-10 18:05:22 +0800

    debug

--- draw_feature_pic.py
+++ draw_feature_pic.py
@@ -140,12 +140,14 @@ def plot_correspondence(data: dict, save_file: Path):
     plt.imshow(cv2.cvtColor(resize1, cv2.COLOR_BGR2RGB))
     plt.axis('off')
     plt.plot(u_r1,v_r1, 'r+', markersize=12, markeredgewidth=2)
-    plt.savefig(str(save_file).replace('.png','_1.png'), dpi=300)
+    plt.savefig(str(save_file).replace('.png','_1.png'),bbox_inches='tight', pad_inches=0, dpi=300)
+    plt.close()
 
     plt.imshow(cv2.cvtColor(resize2, cv2.COLOR_BGR2RGB))
     plt.axis('off')
     plt.plot(u_r2,v_r2, 'r+', markersize=12, markeredgewidth=2)
-    plt.savefig(str(save_file).replace('.png','_2.png'), dpi=300)
+    plt.savefig(str(save_file).replace('.png','_2.png'),bbox_inches='tight', pad_inches=0, dpi=300)
+    plt.close()
     
     # # 竖直堆叠
     # canvas = np.vstack((resize1, resize2))
